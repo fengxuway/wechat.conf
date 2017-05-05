74102b8dcb1ce55504420375d85fe02aa71da0d6
冯旭 <fengxu@xiaoneng.cn>
2017-05-05 15:01:34 +0800

    同步多个nginx推送的缓存数据

--- wechat.lua
+++ wechat.lua
@@ -1,3 +1,8 @@
+local hosts_get = {'http://192.168.30.234:80'}
+local hosts_post = {'http://192.168.30.234:80'}
+local hosts_nginx = {'http://192.168.30.213', 'http://192.168.31.245'}
+
+
 function get_from_cache(key)
     local cache_ngx = ngx.shared.openids
     local value = cache_ngx:get(key)
@@ -21,12 +26,12 @@ function redis_connect()
     local redis = require "resty.redis"
     local red = redis:new()
     red:set_timeout(1000)
-    local ok, err = red:connect("192.168.30.215", 10009)
+    local ok, err = red:connect("127.0.0.1", 6374)
     if not ok then
         ngx.log(ngx.ERR, "failed to connect: ", err)
         return
     end
-    local res, err = red:auth("111111")
+    local res, err = red:auth("rediS")
     if not res then
         ngx.log(ngx.ERR, "failed to authenticate: ", err)
         return
@@ -36,9 +41,9 @@ end
 -- redis中key的前缀
 local red_prefix = "openresty_openid_"
 -- redis缓存的时间 200秒，线上一小时零5分钟
-local expiretime_redis = 200
+local expiretime_redis = 4000
 -- openresty的缓存时间60秒，线上16分钟
-local expiretime = 60     
+local expiretime = 1000     
 function get_wechatid_route(wechatid)
     ngx.log(ngx.INFO, "get_wechatid_route: [",wechatid,"]")
     url = get_from_cache(wechatid)
@@ -51,12 +56,14 @@ function get_wechatid_route(wechatid)
             local all_keys = red:keys(red_prefix.."*")
             for i, k in ipairs(all_keys) do
                 local v = red:get(k)
-                local sub_k = string.sub(k,string.len(red_prefix)+1, string.len(k)-1)
+                local sub_k = string.sub(k,string.len(red_prefix)+1, string.len(k))
                 set_to_cache(sub_k, v, expiretime)
+                ngx.log(ngx.INFO, "set to cache: Key: [", sub_k, "] Value: [",v,"]")
             end
             red:close()
             return get_from_cache(wechatid)
         else
+            ngx.log(ngx.WARN, "Has another key, don't update from redis")
             return url
         end
     else
@@ -65,9 +72,24 @@ function get_wechatid_route(wechatid)
     
 end
 
-local hosts_get = {'http://10.44.48.214:8080'}
-local hosts_post = {'http://10.44.45.145:80', 'http://10.173.27.48:80', 'http://10.171.77.147:80', 'http://10.44.45.145:81'}
 
+function sync(body)
+    local http = require "resty.http"  
+    local httpc = http.new()
+    for i, k in ipairs(hosts_nginx) do
+         local res, err = httpc:request_uri(k..'/api/sync', {  
+            method = "POST",  
+            --args = str,  
+            body = body,  
+            headers = {  
+                ["Content-Type"] = "application/json",  
+            }  
+        })
+        if not res then  
+            ngx.log(ngx.WARN,"failed to request: ", err)  
+        end
+    end
+end
        
 
 local request_method = ngx.var.request_method
@@ -86,19 +108,32 @@ ngx.var.target = hosts_get[1]
 
 
 if match_url then
+
     local wechatid = match_url[1]
-    local ascii = 0
-    len = string.len(wechatid) 
-    for i = 1, len do
-        ascii = ascii + string.byte(wechatid, i)
+    target_url = get_wechatid_route(wechatid)
+    ngx.log(ngx.INFO, "--url: ", target_url)
+
+    if target_url ~= nil then
+        -- 如果存在，跳转到对应缓存中的url
+        ngx.var.target = target_url
+        ngx.log(ngx.INFO, "["..wechatid.."] ===> ", target_url)
+    else
+        local ascii = 0
+        len = string.len(wechatid) 
+        for i = 1, len do
+            ascii = ascii + string.byte(wechatid, i)
+        end
+        mod = ascii % table.getn(hosts_get) + 1
+        ngx.var.target = hosts_get[mod]
+        ngx.log(ngx.INFO, "["..wechatid.."] ===> ", hosts_get[mod])
     end
-    mod = ascii % table.getn(hosts_get) + 1
-    ngx.var.target = hosts_get[mod]
 elseif request_method == "POST" then 
     ngx.req.read_body()
 
+    ngx.log(ngx.INFO, "POST data comming")
     -- 获取各个vpc推送上来的json数据，并写入缓存和redis中
     if uri == "/api/push" then
+        ngx.log(ngx.INFO, "ready to push data")
         local body = ngx.req.get_body_data()
         local cjson = require "cjson"
         local data = cjson.decode(body);
@@ -115,17 +150,37 @@ elseif request_method == "POST" then
             red:expire(red_prefix..v, expiretime_redis)
         end
         red:close()
+
+        sync(body)
+
+        ngx.log(ngx.INFO, "Push Success")
         ngx.say("Push success!")
+        ngx.exit(200)
         return
     end
+
+    if uri == "/api/sync" then
+        local body = ngx.req.get_body_data()
+        ngx.log(ngx.INFO, body)
+        local cjson = require "cjson"
+        local data = cjson.decode(body);
+        for i,v in ipairs(data["openids"]) do
+            set_to_cache(v, data["url"], expiretime)
+            ngx.log(ngx.INFO, v, data['url'])
+        end
+        ngx.exit(200)
+        return
+    end
+
     local data = ngx.req.get_body_data()
     local regex_body = [[<FromUserName>(.*?)</FromUserName>]]
+    local touser_regex_body = [[<ToUserName>(.*?)</ToUserName>]]
     local match_body = ngx.re.match(data, regex_body, "o")
-    if match_body then
-        username = match_body[1]
+    local touser_match_body = ngx.re.match(data, touser_regex_body, "o")
+    if touser_match_body then
+        username = touser_match_body[1]
         -- 如果username被CDATA包裹，则获取其内部的wechatid
         local match_wechatid = ngx.re.match(username, [[<!\[CDATA\[([\w-]+)\]\]>]], "o")
-        ngx.log(ngx.INFO, "--", match_wechatid)
         local wechatid = nil
         if match_wechatid then
             wechatid = match_wechatid[1]
@@ -146,18 +201,28 @@ elseif request_method == "POST" then
             --ngx.say("["..wechatid.."] ===> ", target_url)
         else
             ngx.log(ngx.WARN, "wechatid ["..wechatid.."] not in Cache!")
-            -- 缓存中不存在指定wechatid，则执行原来的逻辑
-            local ascii = 0
-            len = string.len(username) 
-            for i = 1, len do
-                ascii = ascii + string.byte(username, i)
+            if match_body then
+                username = match_body[1]
+                ngx.log(ngx.WARN, "wechatid ["..wechatid.."] not in Cache!")
+                ngx.log(ngx.WARN, "According to FromUserName ["..username.."] Ascii code to random!")
+                -- 缓存中不存在指定wechatid，则执行原来的逻辑
+                local ascii = 0
+                len = string.len(username) 
+                for i = 1, len do
+                    ascii = ascii + string.byte(username, i)
+                end
+                mod = ascii % table.getn(hosts_post) + 1
+                ngx.var.target = hosts_post[mod]
+                ngx.log(ngx.INFO, "["..username.."] ===> ", hosts_post[mod])
+            else
+                ngx.var.target = hosts_post[1]
+                ngx.log(ngx.INFO, "["..username.."] ===> ", hosts_post[mod])
+                
             end
-            mod = ascii % table.getn(hosts_post) + 1
-            ngx.var.target = hosts_post[mod]
-            ngx.log(ngx.INFO, "["..username.."] ===> ", hosts_post[mod])
-        end
+        end    
     else
         ngx.var.target = hosts_post[1]
+        ngx.log(ngx.INFO, "turn to ===> ", hosts_post[1])
     end
 elseif api_match_url then
     ngx.log(ngx.INFO, "API get wechatid bind URL")
@@ -169,6 +234,7 @@ elseif api_match_url then
     else
         ngx.say(url)
     end
+    ngx.exit(200)
 else
     ngx.var.target = hosts_post[1]
 end
